08194805137f407096bc0339f99b6d64b696ee79
Andrés Reyes <p4ter0ski@gmail.com>
2019-09-30 17:56:35 +0200

    [feat] add kata-2 resolution code

--- src/kata-2.js
+++ src/kata-2.js
@@ -1,3 +1,25 @@
 export function kata2 (obj, def, path) {
+  return getObjectPropertyValue(obj, def, path);
+}
 
+const getObjectPropertyValue = (obj, def, path) => {
+
+  const objectToSearchFor = Object.assign(obj, {});
+
+  const readPath = (pathString, obj = objectToSearchFor, defaultValue = def) => {
+    const pathArray = pathString.split('.');
+    const lastKey = pathArray.length - 1;
+    let reducedObject = Object.assign({}, obj);
+
+    return pathArray.map((key, index) => {
+      const value = reducedObject[key];
+      const isLast = index === lastKey;
+      const isObject = typeof value === 'object';
+      reducedObject = isObject && value;
+
+      return isLast && value !== undefined ? value : defaultValue;
+    })[lastKey];
+  }
+
+  return path !== undefined ? readPath(path, objectToSearchFor, def) : readPath;
 }
