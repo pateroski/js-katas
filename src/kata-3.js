e7e76fb4ff7a579b3c4992752a997ceee11779f3
Andrés Reyes <p4ter0ski@gmail.com>
2019-09-30 20:37:50 +0200

    [feat] add kata-3 resolution code

--- src/kata-3.js
+++ src/kata-3.js
@@ -1,3 +1,22 @@
 export function kata3 (n, m) {
-
+    return  getNumbersToTest(n, m)
+            .map((number) => squareDivisorsSumIsASquare(number, getSquareDivisorsSum(number)))
+            .filter((item) => item !== null)
 }
+
+const getNumbersToTest = (startNumber, lastNumber) => {
+  var totalNumbers = (lastNumber - startNumber) + 1;
+  var totalNumbersArray = Array(totalNumbers);
+  return totalNumbersArray.fill().map((item, index) => startNumber + index);
+}
+
+const getSquareDivisorsSum = (number) => {
+  const possibleDivisors = [...Array(number + 1).keys()].slice(1);
+  return possibleDivisors.reduce((sum, possibleDivisor) => {
+    const squareDivisor = !(number % possibleDivisor) && (possibleDivisor * possibleDivisor);
+
+    return sum + squareDivisor;
+  }, 0)
+}
+
+const squareDivisorsSumIsASquare = (number, squareDivisorsSum) => squareDivisorsSum % Math.sqrt(squareDivisorsSum) === 0 ? [number, squareDivisorsSum] : null;
