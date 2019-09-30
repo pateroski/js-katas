895319b52f26413546f8e344fb1fc7122620a364
Andrés Reyes <p4ter0ski@gmail.com>
2019-09-30 16:40:35 +0200

    [feat] add kata-1 resolution code

--- src/kata-1.js
+++ src/kata-1.js
@@ -1,3 +1,10 @@
 export function kata1 (number) {
-
+ return convertIntegerWithDigitsInDescendingOrder(number);
 }
+
+function convertIntegerWithDigitsInDescendingOrder(number) {
+  const stringNumberWithDigitsOrdered = number.toString().split('').sort().reverse().join('');
+
+  return parseInt(stringNumberWithDigitsOrdered) * Math.sign(number);
+}
+
